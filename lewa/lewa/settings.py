f066713a7ce7217cecb55098d6166bff6568bfe9
Brady Fomegne <brady.fomegne@outlook.com>
2025-06-06 01:41:21 +0100

    fix ruff

--- lewa/lewa/settings.py
+++ lewa/lewa/settings.py
@@ -200,7 +200,7 @@ AUTHENTICATION_BACKENDS = (
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_LOGIN_METHODS = {"email"}
-ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*']
+ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*"]
 ACCOUNT_UNIQUE_EMAIL = True
 
 # django-debug-toolbar
