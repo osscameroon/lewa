7b90c26edfed4fb677820501e43fd63ca64c9b73
Brady Fomegne <brady.fomegne@outlook.com>
2025-06-08 01:01:58 +0100

    patch: design the static database (#15)
    This commit add a static database for data like
     information about writing systems, languages, etc.
    
    It don't replace the database who will track
     information about the user.

--- lewa/core/urls.py
+++ lewa/core/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path("typing", views.typing, name="typing"),
     path("pronunciation", views.pronunciation, name="pronunciation"),
     path("languages", views.languages, name="languages"),
+    path("languages/<language>", views.languages, name="languages"),
+    path("writing-systems", views.writing_systems, name="writing-systems"),
+    path(
+        "writing-systems/<writing_system>",
+        views.writing_systems,
+        name="writing-systems",
+    ),
 ]
