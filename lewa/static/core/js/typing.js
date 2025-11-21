6f1b38db51c94d443fd7986e958e5b1b8f9244de
Rebel Zhang <rebel1725@autistici.org>
2025-11-22 02:24:37 +0800

    Added speed refreshing logic

--- lewa/static/core/js/typing.js
+++ lewa/static/core/js/typing.js
@@ -1,9 +1,57 @@
+const input = document.getElementById('user-input')
+const div_speed = document.getElementById('div-speed')
+const div_accuracy = document.getElementById('div-accuracy')
+const timer = document.getElementById('timer')
+const start = performance.now()
+
+function elapsed()
+{
+    return performance.now() - start;
+}
+
+async function updateLoop(interval = 200) {
+    while (true) {
+	timer.innerHTML = formatMsToMMSS(elapsed());
+        await new Promise(resolve => setTimeout(resolve, interval));
+    }
+}
+
+updateLoop();
+
+function formatMsToMMSS(ms) {
+    const totalSeconds = Math.floor(ms / 1000);
+
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const mm = String(minutes).padStart(2, '0');
+    const ss = String(seconds).padStart(2, '0');
+
+    return `${mm}:${ss}`;
+}
+
 function clearInput()
 {
-    document.getElementById('user-input').value = '';
+    input.value = '';
 }
 
 function resetInput()
 {
     clearInput();
 }
+
+input.addEventListener('input', function (event) {
+    const wpm = Math.round((input.value.length * 60000) / elapsed());
+    const wpmstr = wpm.toString();
+    let prog = Math.round(wpm/2);
+    if (prog>100) prog=100;
+    div_speed.innerHTML = `<div class="column is-2">
+                        <span class="has-text-weight-semibold">Speed</span>
+                    </div>
+                    <div class="column">
+                        <progress class="progress is-info" value="`+prog.toString()+`" max="100">`+prog.toString()+`%</progress>
+                    </div>
+                    <div class="column is-1 has-text-right">
+                        <span class="has-text-weight-semibold">`+wpmstr+` WPM</span>
+                    </div>`
+});
