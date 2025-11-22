453d073dcee00bb9a24b7d6f94eb29ab22bbd5e9
@ntoinet <antoine.brunel@gmail.com>
2025-11-22 23:01:22 +0100

    Implement (free) Google Authentication (#31)
    * Fix: Add missing initial migration for accounts app and configure Auth
    
    * Fix: Add missing settings, and app config
    
    * fix: Remove initial migration file
    
    ---------
    
    Co-authored-by: Antoine Brunel <antoinebrunel@MacBook-Air-de-Antoine.local>

--- lewa/accounts/models.py
+++ lewa/accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     """Custom user type; can get email by str(custom_user_instance)"""
     def __str__(self):
-        return self.email
+        return self.email or self.username
