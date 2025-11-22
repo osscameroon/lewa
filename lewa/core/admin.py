7b25d0ba8d6fa3dd58b06719dfac61dfc8fef3be
Ishuman <ishuman2006@gmail.com>
2025-11-23 03:13:04 +0530

    enable Score model in Admin

--- lewa/core/admin.py
+++ lewa/core/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
-# from .models import Score
+from django.contrib import admin
+from .models import Score
 
-# admin.site.register(Score)
+admin.site.register(Score)
