d6882ad9a27c33d2c08f7bc05aa828d86e90d83d
Rebel Zhang <rebel1725@autistici.org>
2025-11-22 18:35:14 +0800

    Added accuracy logic

--- lewa/static/core/js/typing.js
+++ lewa/static/core/js/typing.js
@@ -4,6 +4,25 @@ const div_accuracy = document.getElementById('div-accuracy')
 const timer = document.getElementById('timer')
 const start = performance.now()
 
+function min(a, b) {
+  return a < b ? a : b;
+}
+
+function accuracy(str1, str2)
+{
+    if (str2.length==0) return 0;
+    minlength=min(str1.length, str2.length);
+    let correctcount=0;
+    for (let i1=0;i1<minlength;i1++)
+    {
+	if (str1.charAt(i1)==str2.charAt(i1))
+	{
+	    correctcount++;
+	}
+    }
+    return correctcount/str2.length;
+}
+
 function elapsed()
 {
     return performance.now() - start;
@@ -45,6 +64,8 @@ input.addEventListener('input', function (event) {
     const wpmstr = wpm.toString();
     let prog = Math.round(wpm/2);
     if (prog>100) prog=100;
+    agstr = document.getElementById("lesson-text").innerText;
+    let acc = Math.round(accuracy(agstr, input.value)*100).toString();
     div_speed.innerHTML = `<div class="column is-2">
                         <span class="has-text-weight-semibold">Speed</span>
                     </div>
@@ -53,5 +74,14 @@ input.addEventListener('input', function (event) {
                     </div>
                     <div class="column is-1 has-text-right">
                         <span class="has-text-weight-semibold">`+wpmstr+` WPM</span>
-                    </div>`
+                    </div>`;
+    div_accuracy.innerHTML = `<div class="column is-2">
+                        <span class="has-text-weight-semibold">Accuracy</span>
+                    </div>
+                    <div class="column">
+                        <progress class="progress is-success" value="`+acc+`" max="100">`+acc+`%</progress>
+                    </div>
+                    <div class="column is-1 has-text-right">
+                        <span class="has-text-weight-semibold">`+acc+`%</span>
+                    </div>`;
 });
