5be75d0c526cd2b678e0b9015135259a02d40720
arthurhb <arthurhb@sdf.org>
2025-11-22 11:51:26 -0300

    day 2: attemp to fix CI errors

--- lewa/accounts/models.py
+++ lewa/accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomUser(AbstractUser):
-    
     def __str__(self):
         return self.email
